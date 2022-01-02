b44c56d5b8e5fae6fb1bf1b1ed783d6decc1ac06
kool7d <kool7d@gmail.com>
2022-01-01 22:42:11 -0600

    doc3

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 using Documenter, WGLMakie, JSServe
 
 makedocs(; modules=[WGLMakie], format=Documenter.HTML(), pages=["Home" => "index.md"],
-         repo="https://github.com/JuliaPlots/WGLMakie.jl/blob/{commit}{path}#L{line}",
-         sitename="WGLMakie.jl", authors="Simon Danisch")
+         repo="https://github.com/kool7d/WGLMakie.jl/blob/{commit}{path}#L{line}",
+         sitename="WGLMakie.jl", authors="Dan Kool")
 
-deploydocs(; repo="github.com/JuliaPlots/WGLMakie.jl", push_preview=true)
+deploydocs(; repo="github.com/kool7d/WGLMakie.jl", push_preview=true)
